f532873f232a52684883cbdc52451eaf25073cb1
RGMaddy <rosalinda.gonzales@inacapmail.cl>
2025-05-05 12:38:50 -0400

    Initial commit

---
+++ assets/js/scriptIndex.js
@@ -0,0 +1,31 @@
+function validar(){
+    let eNombre = document.getElementById("nombre")
+    let vNombre = eNombre.value
+    let eErrorNombre = document.getElementById("error nombre")
+
+    let eApellido = document.getElementById("apellido")
+    let vApellido = eNombre.value
+    let eErrorApellido = document.getElementById("error apellido")
+
+    validarLargoMinimo(eNombre,vNombre,eErrorNombre)
+    validarLargoMinimo(eApellido,vApellido,eErrorApellido)
+
+    function validarLargoMinimo(elemento,valor,eError){
+        if(valor.length < 3 ){
+            console.log("pocos caracteres")
+            alert("debes ingresar mas de 3 caractres")
+            eErrorNombre.innerText = "debes ingresar mas de 3 caracteres!"
+            eNombre.style.backgroundColor="red"
+            eNombre.style.color="white"
+        }else{
+            console.log("todo bien")
+            eErrorNombre.innerText=""
+            eNombre.style.backgroundColor="green"
+        }
+    }
+   
+}
+
+function validarLargoMinimo(valor){
+
+}
