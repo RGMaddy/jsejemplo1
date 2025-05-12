4439f6066a502271f8ca354093ea8e7f28f29eef
RGMaddy <rosalinda.gonzales@inacapmail.cl>
2025-05-12 11:38:30 -0400

    comit de prueba

--- assets/js/scriptIndex.js
+++ assets/js/scriptIndex.js
@@ -1,3 +1,4 @@
+let personas =[]
 function validar(){
     let eNombre = document.getElementById("nombre")
     let vNombre = eNombre.value
@@ -10,20 +11,20 @@ function validar(){
     validarLargoMinimo(eNombre,vNombre,eErrorNombre)
     validarLargoMinimo(eApellido,vApellido,eErrorApellido)
 
-    function validarLargoMinimo(elemento,valor,eError){
-        if(valor.length < 3 ){
-            console.log("pocos caracteres")
-            alert("debes ingresar mas de 3 caractres")
-            eErrorNombre.innerText = "debes ingresar mas de 3 caracteres!"
-            eNombre.style.backgroundColor="red"
-            eNombre.style.color="white"
-        }else{
-            console.log("todo bien")
-            eErrorNombre.innerText=""
-            eNombre.style.backgroundColor="green"
-        }
+}
+function validarLargoMinimo(elemento,valor,eError){
+    if(valor.length < 3 ){
+        console.log("pocos caracteres")
+        alert("debes ingresar mas de 3 caractres")
+        eErrorNombre.innerText = "debes ingresar mas de 3 caracteres!"
+        eNombre.style.backgroundColor="red"
+        eNombre.style.color="white"
+        return"falla"
+    }else{
+        console.log("todo bien")
+        eErrorNombre.innerText=""
+        eNombre.style.backgroundColor="green"
     }
-   
 }
 
 function validarLargoMinimo(valor){
